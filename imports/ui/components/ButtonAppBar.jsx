1c145127cabf9b4bc301f41b1183f01ac122b596
Haris Ljajic <haris.ljajic@gmail.com>
2019-06-20 18:57:50 +0200

    Add navigation drawer

--- imports/ui/components/ButtonAppBar.jsx
+++ imports/ui/components/ButtonAppBar.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
+import Button from '@material-ui/core/Button';
+import Divider from '@material-ui/core/Divider';
+import Drawer from '@material-ui/core/Drawer';
+import IconButton from '@material-ui/core/IconButton';
+import People from '@material-ui/icons/People';
+import PeopleOutline from '@material-ui/icons/PeopleOutline';
+import List from '@material-ui/core/List';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
+import MenuIcon from '@material-ui/icons/Menu';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
-import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
-import MenuIcon from '@material-ui/icons/Menu';
 
 const useStyles = makeStyles(theme => ({
+  list: {
+    width: 250,
+  },
   root: {
     flexGrow: 1,
   },
@@ -15,32 +26,76 @@ const useStyles = makeStyles(theme => ({
     marginRight: theme.spacing(2),
   },
   title: {
-    flexGrow: 1,
-  },
+    flexGrow: 1
+  }
 }));
 
-function ButtonAppBar() {
+export default function ButtonAppBar() {
   const classes = useStyles();
-  const logoStyle = {
-    width: 40,
-    height: 40
+
+  const [state, setState] = React.useState({
+    top: false,
+    left: false,
+    bottom: false,
+    right: false,
+  });
+
+  const toggleDrawer = (side, open) => event => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+
+    setState({ ...state, [side]: open });
   };
+
+  const imageCenter = {
+    "display": "block",
+    "margin-top": "20px",
+    "margin-left": "auto",
+    "margin-right": "auto",
+    "width": "125px",
+    "height": "125px"
+  };
+
+  const sideList = side => (
+    <div
+      className={classes.list}
+      role="presentation"
+      onClick={toggleDrawer(side, false)}
+      onKeyDown={toggleDrawer(side, false)}
+    >
+      <img src="/logo.jpg" style={imageCenter} />
+
+      <List>
+        {['Jetimi', 'Donatori'].map((text, index) => (
+          <ListItem button key={text}>
+            <ListItemIcon>{index % 2 === 0 ? <People /> : <PeopleOutline />}</ListItemIcon>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+      <Divider />
+    </div>
+  );
 
   return (
     <div className={classes.root}>
-      <AppBar position="static" color="white">
+      <AppBar position="static">
         <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu">
-            <img src="/logo.jpg" style={logoStyle} />
+          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="Menu"
+            onClick={toggleDrawer('left', true)}>
+            <MenuIcon />
           </IconButton>
-          <Button color="inherit">Jetimi</Button>
-          <Button color="inherit" >Donatori</Button>
-          <Typography variant="h6" className={classes.title} />
+          <Typography variant="h6" className={classes.title}>
+            Jetimi Sandžaka
+          </Typography>
           <Button color="inherit">Login</Button>
         </Toolbar>
       </AppBar>
+
+      <Drawer open={state.left} onClose={toggleDrawer('left', false)}>
+        {sideList('left')}
+      </Drawer>
     </div>
   );
 }
-
-export default ButtonAppBar;
---
+++ imports/ui/pages/Child/AddChild.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+export default class AddChild extends React.Component {
+  render() {
+    return (
+      null
+    );
+  }
+}
