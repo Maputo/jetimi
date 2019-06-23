645a09c5721551577e7085dac8716cb1afb3974a
Haris Ljajic <haris.ljajic@gmail.com>
2019-06-23 23:07:40 +0200

    Enable linking row to profile page

--- imports/ui/pages/Child/Children.jsx
+++ imports/ui/pages/Child/Children.jsx
@@ -1,68 +1,90 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import MaterialTable from 'material-table';
+import Redirect from 'react-router-dom';
 
-export default function Children() {
+export default class Children extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = {
+      columns: [
+        { title: 'Name', field: 'name' },
+        { title: 'Surname', field: 'surname' },
+        { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
+        {
+          title: 'Birth Place',
+          field: 'birthCity',
+          lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
+        },
+      ],
+      data: [
+        { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
+        {
+          name: 'Zerya Betül',
+          surname: 'Baran',
+          birthYear: 2017,
+          birthCity: 34,
+        },
+      ],
+    };
+  }
 
-  const [state, setState] = React.useState({
-    columns: [
-      { title: 'Name', field: 'name' },
-      { title: 'Surname', field: 'surname' },
-      { title: 'Birth Year', field: 'birthYear', type: 'numeric' },
-      {
-        title: 'Birth Place',
-        field: 'birthCity',
-        lookup: { 34: 'İstanbul', 63: 'Şanlıurfa' },
-      },
-    ],
-    data: [
-      { name: 'Mehmet', surname: 'Baran', birthYear: 1987, birthCity: 63 },
-      {
-        name: 'Zerya Betül',
-        surname: 'Baran',
-        birthYear: 2017,
-        birthCity: 34,
-      },
-    ],
-  });
+  static contextTypes = {
+    router: PropTypes.shape({
+      history: PropTypes.shape({
+        push: PropTypes.func.isRequired,
+        replace: PropTypes.func.isRequired
+      }).isRequired,
+      staticContext: PropTypes.object
+    }).isRequired
+  };
 
-  return (
-    <div className="children">
-      <h1>Children</h1>
+  handleOnClick = (event) => {
+    console.log('Row click event: ', event);
+    this.context.router.history.push('/child/profile');
+  };
+
+  render() {
+    console.log(this.context);
+    return (
+      <div className="children">
         <MaterialTable
-          title="Editable Example"
-          columns={state.columns}
-          data={state.data}
+          title="Jetimi"
+          columns={this.state.columns}
+          data={this.state.data}
           editable={{
             onRowAdd: newData =>
               new Promise(resolve => {
                 setTimeout(() => {
                   resolve();
-                  const data = [...state.data];
+                  const data = [...this.state.data];
                   data.push(newData);
-                  setState({ ...state, data });
+                  this.setState({ ...this.state, data });
                 }, 600);
               }),
             onRowUpdate: (newData, oldData) =>
               new Promise(resolve => {
                 setTimeout(() => {
                   resolve();
-                  const data = [...state.data];
+                  const data = [...this.state.data];
                   data[data.indexOf(oldData)] = newData;
-                  setState({ ...state, data });
+                  this.setState({ ...this.state, data });
                 }, 600);
               }),
             onRowDelete: oldData =>
               new Promise(resolve => {
                 setTimeout(() => {
                   resolve();
-                  const data = [...state.data];
+                  const data = [...this.state.data];
                   data.splice(data.indexOf(oldData), 1);
-                  setState({ ...state, data });
+                  this.setState({ ...this.state, data });
                 }, 600);
               }),
           }}
+          onRowClick={this.handleOnClick}
         />
-    </div>
-  );
+      </div>
+    );
+  }
 }
