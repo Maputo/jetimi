9fb6d2baf00a41b15a879d6178f8b84404b4cf63
Haris Ljajic <haris.ljajic@gmail.com>
2020-01-19 21:07:02 +0100

    Move table to a separate component

--- imports/ui/pages/Protege/ProtegesPage.jsx
+++ imports/ui/pages/Protege/ProtegesPage.jsx
@@ -2,83 +2,14 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { withRouter } from 'react-router';
 import { makeStyles } from '@material-ui/core/styles';
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TablePagination from '@material-ui/core/TablePagination';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
-import EnhancedTableHead from '../../components/molecules/EnhancedTableHead.jsx';
 import FiltersAndChipsContainer from '../../containers/FiltersAndChipsContainer.jsx';
 import BottomAppBar from '../../components/molecules/BottomAppBar.jsx';
+import CustomTable from '../../components/atoms/CustomTable.jsx';
 import { EMPTY_ARRAY, EMPTY_OBJECT } from '../../../../utils/DefaultProps.js';
-
-function desc(a, b, orderBy) {
-  if (b[orderBy] < a[orderBy]) {
-    return -1;
-  }
-  if (b[orderBy] > a[orderBy]) {
-    return 1;
-  }
-  return 0;
-}
-
-function stableSort(array, cmp) {
-  const stabilizedThis = array.map((el, index) => [el, index]);
-  stabilizedThis.sort((a, b) => {
-    const order = cmp(a[0], b[0]);
-    if (order !== 0) return order;
-    return a[1] - b[1];
-  });
-  return stabilizedThis.map((el) => el[0]);
-}
-
-function getSorting(order, orderBy) {
-  return order === 'desc' ? (a, b) => desc(a, b, orderBy) : (a, b) => -desc(a, b, orderBy);
-}
-
-const headCells = [
-  {
-    id: 'name', numeric: false, disablePadding: true, label: 'Ime i prezime',
-  },
-  {
-    id: 'age', numeric: true, disablePadding: false, label: 'Starost',
-  },
-  {
-    id: 'address', numeric: false, disablePadding: false, label: 'Adresa',
-  },
-  {
-    id: 'sponsor', numeric: false, disablePadding: false, label: 'Sponzor',
-  },
-  {
-    id: 'situation', numeric: true, disablePadding: false, label: 'Situacija',
-  },
-];
 
 const useStyles = makeStyles((theme) => ({
   root: {
     width: '100%',
-  },
-  paper: {
-    width: '100%',
-    marginBottom: theme.spacing(2),
-  },
-  table: {
-    minWidth: 750,
-  },
-  tableWrapper: {
-    overflowX: 'auto',
-  },
-  visuallyHidden: {
-    border: 0,
-    clip: 'rect(0 0 0 0)',
-    height: 1,
-    margin: -1,
-    overflow: 'hidden',
-    padding: 0,
-    position: 'absolute',
-    top: 20,
-    width: 1,
   },
   filler: {
     height: theme.spacing(6),
@@ -87,101 +18,16 @@ const useStyles = makeStyles((theme) => ({
 
 const ProtegesPage = (props) => {
   const classes = useStyles();
-  const [order, setOrder] = React.useState('asc');
-  const [orderBy, setOrderBy] = React.useState('age');
-  const [page, setPage] = React.useState(0);
-  const [rowsPerPage, setRowsPerPage] = React.useState(10);
-
   const { proteges = [], history } = props;
-
-  const handleRequestSort = (event, property) => {
-    const isDesc = orderBy === property && order === 'desc';
-    setOrder(isDesc ? 'asc' : 'desc');
-    setOrderBy(property);
-  };
 
   const handleClick = (event, name) => { // eslint-disable-line no-unused-vars
     history.push('/p/profile');
   };
 
-  const handleChangePage = (event, newPage) => {
-    setPage(newPage);
-  };
-
-  const handleChangeRowsPerPage = (event) => {
-    const rowsSelection = parseInt(event.target.value, 10);
-    setRowsPerPage(rowsSelection);
-    setPage(0);
-  };
-
-  const emptyRows = rowsPerPage - Math.min(rowsPerPage, proteges.length - page * rowsPerPage);
-
   return (
     <div className={classes.root}>
-      <Paper className={classes.paper}>
-        <FiltersAndChipsContainer />
-        <div className={classes.tableWrapper}>
-          <Table
-            className={classes.table}
-            aria-labelledby="tableTitle"
-            size="medium"
-            aria-label="enhanced table"
-          >
-            <EnhancedTableHead
-              classes={classes}
-              order={order}
-              orderBy={orderBy}
-              onRequestSort={handleRequestSort}
-              headCells={headCells}
-            />
-            <TableBody>
-              {stableSort(proteges, getSorting(order, orderBy))
-                .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
-                .map((row, index) => {
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
-                  return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      tabIndex={-1}
-                      key={row.name}
-                    >
-                      <TableCell padding="checkbox" />
-                      <TableCell component="th" id={labelId} scope="row" padding="none">
-                        {row.name}
-                      </TableCell>
-                      <TableCell
-                        style={{ width: '10%' }}
-                        align="right"
-                      >
-                        {row.age}
-                      </TableCell>
-                      <TableCell style={{ width: '40%' }} align="left">{row.address}</TableCell>
-                      <TableCell style={{ width: '10%' }} align="left">{row.sponsor}</TableCell>
-                      <TableCell style={{ width: '10%' }} align="right">{row.situation}</TableCell>
-                    </TableRow>
-                  );
-                })}
-              {emptyRows > 0 && (
-                <TableRow style={{ height: 53 }}>
-                  <TableCell colSpan={6} />
-                </TableRow>
-              )}
-            </TableBody>
-          </Table>
-        </div>
-        <TablePagination
-          rowsPerPageOptions={[10, 25, 50]}
-          component="div"
-          count={proteges.length}
-          rowsPerPage={rowsPerPage}
-          page={page}
-          onChangePage={handleChangePage}
-          onChangeRowsPerPage={handleChangeRowsPerPage}
-        />
-      </Paper>
+      <FiltersAndChipsContainer />
+      <CustomTable entries={proteges} onRowClick={handleClick} />
       <div className={classes.filler} />
       <BottomAppBar />
     </div>
