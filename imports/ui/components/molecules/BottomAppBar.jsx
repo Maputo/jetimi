8c4c2a3ed6b91368c1b9597768f6832e20782f23
Haris Ljajic <haris.ljajic@gmail.com>
2020-01-22 23:52:26 +0100

    Add more icons to bottom app bar

--- imports/ui/components/molecules/BottomAppBar.jsx
+++ imports/ui/components/molecules/BottomAppBar.jsx
@@ -6,34 +6,95 @@ import CssBaseline from '@material-ui/core/CssBaseline';
 import Toolbar from '@material-ui/core/Toolbar';
 import Fab from '@material-ui/core/Fab';
 import AddIcon from '@material-ui/icons/Add';
+import EditIcon from '@material-ui/icons/Edit';
+import CloseIcon from '@material-ui/icons/Close';
+import CheckIcon from '@material-ui/icons/Check';
 
 const useStyles = makeStyles((theme) => ({
   appBar: {
     top: 'auto',
     bottom: 0,
   },
-  fabButton: {
+  fabContainer: {
     position: 'absolute',
     zIndex: 1,
     top: -15,
     left: 0,
     right: 0,
     margin: '0 auto',
+    display: 'flex',
+    justifyContent: 'center',
+  },
+  fabButton: {
+    marginRight: theme.spacing(1),
+    marginLeft: theme.spacing(1),
   },
 }));
 
 export default function BottomAppBar(props) {
   const classes = useStyles();
-  const { onAdd } = props;
+  const { onAdd, onEdit, onCancel, onConfirm } = props;
+
+  const renderAddIcon = () => {
+    if (onAdd) {
+      return (
+        <Fab color="secondary" aria-label="add" className={classes.fabButton}>
+          <AddIcon onClick={onAdd} />
+        </Fab>
+      );
+    }
+
+    return null;
+  };
+
+  const renderEditIcon = () => {
+    if (onEdit) {
+      return (
+        <Fab color="secondary" aria-label="edit" className={classes.fabButton}>
+          <EditIcon onClick={onEdit} />
+        </Fab>
+      );
+    }
+
+    return null;
+  };
+
+  const renderCancelIcon = () => {
+    if (onCancel) {
+      return (
+        <Fab color="secondary" aria-label="edit" className={classes.fabButton}>
+          <CloseIcon onClick={onCancel} />
+        </Fab>
+      );
+    }
+
+    return null;
+  };
+
+  const renderConfirmIcon = () => {
+    if (onConfirm) {
+      return (
+        <Fab color="secondary" aria-label="edit" className={classes.fabButton}>
+          <CheckIcon onClick={onConfirm} />
+        </Fab>
+      );
+    }
+
+    return null;
+  };
 
   return (
     <React.Fragment>
       <CssBaseline />
       <AppBar position="fixed" color="primary" className={classes.appBar}>
         <Toolbar>
-          <Fab color="secondary" aria-label="add" className={classes.fabButton}>
-            <AddIcon onClick={onAdd} />
-          </Fab>
+          <div className={classes.fabContainer}>
+            {renderAddIcon()}
+            {renderEditIcon()}
+            {renderCancelIcon()}
+            {renderConfirmIcon()}
+          </div>
+
         </Toolbar>
       </AppBar>
     </React.Fragment>
@@ -41,5 +102,15 @@ export default function BottomAppBar(props) {
 }
 
 BottomAppBar.propTypes = {
-  onAdd: PropTypes.func.isRequired,
+  onAdd: PropTypes.func,
+  onEdit: PropTypes.func,
+  onCancel: PropTypes.func,
+  onConfirm: PropTypes.func,
+};
+
+BottomAppBar.defaultProps = {
+  onAdd: null,
+  onEdit: null,
+  onCancel: null,
+  onConfirm: null,
 };
