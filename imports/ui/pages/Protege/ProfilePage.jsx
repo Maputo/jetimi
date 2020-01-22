6c410efe59e24fdc9b5391885cfe1033b5b14f48
Haris Ljajic <haris.ljajic@gmail.com>
2020-01-22 19:45:38 +0100

    Finish profile page for now

--- imports/ui/pages/Protege/ProfilePage.jsx
+++ imports/ui/pages/Protege/ProfilePage.jsx
@@ -9,8 +9,8 @@ import FormControl from '@material-ui/core/FormControl';
 import FormLabel from '@material-ui/core/FormLabel';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Gender from '../../../../utils/GenderConstants.js';
 import Radio from '@material-ui/core/Radio';
+import Gender from '../../../../utils/GenderConstants.js';
 
 const useStylesAvatar = makeStyles((theme) => ({
   root: {
@@ -25,33 +25,48 @@ const useStylesAvatar = makeStyles((theme) => ({
     height: theme.spacing(3),
   },
   large: {
-    width: theme.spacing(30),
-    height: theme.spacing(30),
+    width: theme.spacing(40),
+    height: theme.spacing(40),
   },
 }));
 
 const useStyles = makeStyles((theme) => ({
   root: {
+    padding: 0,
+    margin: 0,
+    listStyle: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+
     '& .MuiTextField-root': {
-      margin: theme.spacing(0.50, 2),
-      minWidth: 300,
+      marginLeft: theme.spacing(1),
+      marginTop: theme.spacing(1),
+      width: '95%',
     },
     '& .MuiTextField-multiline': {
       fontSize: 7,
+      width: '100%',
+    },
+    '& .MuiInputBase-root': {
+      width: '100%',
     },
   },
   formControl: {
-    margin: theme.spacing(0.5, 2),
+    margin: theme.spacing(1, 2),
+  },
+  text: {
+    display: 'block',
   },
   buttons: {
     display: 'flex',
     flex: '1 1 100%',
     flexDirection: 'row',
     justifyContent: 'flex-end',
-    margin: theme.spacing(2, 2),
+    marginTop: theme.spacing(2),
 
     '& > *': {
-      width: 135,
+      width: 'auto',
       marginLeft: theme.spacing(2),
     },
   },
@@ -59,12 +74,11 @@ const useStyles = makeStyles((theme) => ({
     display: 'flex',
     flex: '1 1 100%',
     flexDirection: 'row',
-    margin: theme.spacing(2, 2),
     flexWrap: 'wrap',
   },
   card: {
-    maxWidth: 500,
-    margin: 10,
+    padding: theme.spacing(2),
+    width: theme.spacing(45),
   },
 }));
 
@@ -93,45 +107,47 @@ export default function ProfilePage() {
               <Avatar alt="Jetimi Sandžaka" src="/logo.jpg" className={avatarClasses.large} />
             </div>
             <div>
-              <TextField required id="name" label="Ime i prezime" />
-              <TextField required id="address" label="Adresa" />
-              <TextField required id="town" label="Grad" />
+              <TextField className={classes.text} required id="name" label="Ime i prezime" />
+              <TextField className={classes.text} required id="address" label="Adresa" />
+              <TextField className={classes.text} required id="town" label="Grad" />
             </div>
           </Paper>
 
           <Paper className={classes.card} variant="elevation" elevation={0}>
-            <TextField required id="date-of-birth" label="Datum rođenja" type="date"
-                       InputLabelProps={{ shrink: true }} />
-            <TextField id="join-date" label="Datum pristupa" type="date" InputLabelProps={{ shrink: true }} />
-            <FormControl component="fieldset" className={classes.formControl}>
-              <FormLabel component="legend">Pol</FormLabel>
-              <RadioGroup aria-label="gender" name="gender" value={gender} onChange={handleGenderChange}>
-                <FormControlLabel value={Gender.m.id} control={<Radio />} label={Gender.m.label} />
-                <FormControlLabel value={Gender.f.id} control={<Radio />} label={Gender.f.label} />
-              </RadioGroup>
-            </FormControl>
-            <FormControl component="fieldset" className={classes.formControl}>
-              <FormLabel component="legend">Ima sponzora</FormLabel>
-              <RadioGroup aria-label="gender" name="sponsor" value={sponsor ? 'Da' : 'Ne'}
-                          onChange={handleSponsorChange}>
-                <FormControlLabel value="Da" control={<Radio />} label="Da" />
-                <FormControlLabel value="Ne" control={<Radio />} label="Ne" />
-              </RadioGroup>
-            </FormControl>
+            <TextField className={classes.text} required id="date-of-birth" label="Datum rođenja" type="date" InputLabelProps={{ shrink: true }} />
+            <TextField className={classes.text} id="join-date" label="Datum pristupa" type="date" InputLabelProps={{ shrink: true }} />
+            <div className={classes.text}>
+              <div style={{ display: 'flex', justifyContent: 'space-between' }}>
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Pol</FormLabel>
+                <RadioGroup aria-label="gender" name="gender" value={gender} onChange={handleGenderChange}>
+                  <FormControlLabel value={Gender.m.id} control={<Radio />} label={Gender.m.label} />
+                  <FormControlLabel value={Gender.f.id} control={<Radio />} label={Gender.f.label} />
+                </RadioGroup>
+              </FormControl>
+              <FormControl component="fieldset" className={classes.formControl}>
+                <FormLabel component="legend">Ima sponzora</FormLabel>
+                <RadioGroup aria-label="gender" name="sponsor" value={sponsor ? 'Da' : 'Ne'} onChange={handleSponsorChange}>
+                  <FormControlLabel value="Da" control={<Radio />} label="Da" />
+                  <FormControlLabel value="Ne" control={<Radio />} label="Ne" />
+                </RadioGroup>
+              </FormControl>
+              </div>
+            </div>
             <TextField
               id="additionalInfo"
               label="Dodatne informacije"
               multiline
               variant="filled"
             />
-            <div className={classes.buttons}>
-              <Button variant="outlined" color="default">
-                Odustani
-              </Button>
-              <Button variant="contained" color="primary">
-                Potvrdi
-              </Button>
-            </div>
+            {/*<div className={classes.buttons}>*/}
+            {/*  <Button variant="outlined" color="default">*/}
+            {/*    Odustani*/}
+            {/*  </Button>*/}
+            {/*  <Button variant="contained" color="primary">*/}
+            {/*    Potvrdi*/}
+            {/*  </Button>*/}
+            {/*</div>*/}
           </Paper>
         </div>
       </form>
