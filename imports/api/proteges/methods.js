fecb50db01fe3e850ca3ccf9233defcd6d30be0b
Haris Ljajic <haris.ljajic@gmail.com>
2020-01-25 00:52:34 +0100

    Update proteges collection and add methods

--- imports/api/proteges/methods.js
+++ imports/api/proteges/methods.js
@@ -1,8 +1,46 @@
 import { Meteor } from 'meteor/meteor';
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
+import { DDPRateLimiter } from 'meteor/ddp-rate-limiter';
+import { contains, pluck } from 'underscore';
+import { Mongo } from 'meteor/mongo';
+
 import Proteges from './proteges.js';
 
-Meteor.methods({
-  'proteges.update'({ id, obj }) {
-    Proteges.update(id, { $set: obj });
+const update = new ValidatedMethod({
+  name: 'proteges.update',
+  validate: new SimpleSchema({
+    id: { type: String },
+    name: { type: String },
+  }).validator(),
+  run({ id, name }) {
+    const mongoId = new Mongo.Collection.ObjectID(id);
+    Proteges.update(mongoId, {
+      $set: { name },
+    });
   },
 });
+
+
+// Get list of all method names on Lists
+const LISTS_METHODS = pluck([
+  update,
+], 'name');
+
+if (Meteor.isServer) {
+  // Only allow 5 protege operations per connection per second
+  DDPRateLimiter.addRule({
+    name(name) {
+      return contains(LISTS_METHODS, name);
+    },
+
+    // Rate limit per connection ID
+    connectionId() {
+      return true;
+    },
+  }, 5, 1000);
+}
+
+export {
+  update,
+};
--- imports/api/proteges/proteges.js
+++ imports/api/proteges/proteges.js
@@ -1,13 +1,57 @@
 import { Mongo } from 'meteor/mongo';
+import { SimpleSchema } from 'meteor/aldeed:simple-schema';
 
-const Proteges = new Mongo.Collection('Proteges');
+class ProtegesCollection extends Mongo.Collection {
+  insert(protege, callback) {
+    return super.insert(protege, callback);
+  }
+
+  update(selector, modifier) {
+    return super.update(selector, modifier);
+  }
+
+  remove(selector) {
+    return super.remove(selector);
+  }
+}
+
+const Proteges = new ProtegesCollection('Proteges');
+
+Proteges.deny({
+  insert() {
+    return true;
+  },
+  update() {
+    return true;
+  },
+  remove() {
+    return true;
+  },
+});
+
+Proteges.schema = new SimpleSchema({
+  _id: { type: String },
+  name: { type: String, optional: true },
+  dateOfBirth: { type: Date, optional: true },
+  joindate: { type: Date, optional: true },
+  addressId: { type: String, optional: true },
+  text: { type: String, optional: true },
+  sponsor: { type: Boolean, optional: true },
+  situation: { type: Number, optional: true },
+  gender: { type: String, optional: true },
+});
+
+Proteges.attachSchema(Proteges.schema);
 
 Proteges.publicFields = {
   name: 1,
   dateOfBirth: 1,
+  joinDate: 1,
   address: 1,
   sponsor: 1,
   situation: 1,
+  text: 1,
+  gender: 1,
 };
 
 export default Proteges;
